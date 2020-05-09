e7fcf173a78dfbd682a07ed23bee1456b782bfd3
Thierno <daoudayaboully@yahoo.fr>
2020-05-09 09:43:10 -0400

    Adding two model to the classifier.

--- ML/main.py
+++ ML/main.py
@@ -4,6 +4,6 @@ from ML import training_testing, data_preprocess
 
 df, y_column = data_preprocess.data_preprocess1()
 
-model, model_perf = training_testing.train_and_test(df, y_column, 'Logistic Regression', 0.3)
+model, model_perf = training_testing.train_and_test(df, y_column, 'Support Vector Machine', 0.3)
 
 
--- ML/model_constants.py
+++ ML/model_constants.py
@@ -1,17 +1,27 @@
 # here import the model to be called
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 # model types
 models = {
-    'LR': LogisticRegression(solver='lbfgs', multi_class='multinomial', penalty='l1')
+    'LR': LogisticRegression(solver='lbfgs', multi_class='multinomial', penalty='l1'),
+    'RF': RandomForestClassifier(n_estimators=40),
+    'SVM': LinearSVC()
 }
 
 model_names = [
-    {'value': 'LR', 'label': 'Logistic Regression'}
+    {'value': 'LR', 'label': 'Logistic Regression'},
+    {'value': 'RF', 'label': 'Random Forest'},
+    {'value': 'SVM', 'label': 'Support Vector Machine'}
 ]
 
 # tuning_param
 
 tuning_params = {
-    'LR': {'penalty': ['l2'], 'C': [0.0001, 0.001, 0.01]}
+    'LR': {'penalty': ['l2'], 'C': [0.0001, 0.001, 0.01]},
+    'RF': {'n_estimators': [1, 20, 30, 40, 50]},
+    'SVM': {'C': [0.001, 0.01, 0.1, 1]}
 }
 
