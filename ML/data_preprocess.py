3977c92d839fb664aa691909a8510c51f889ada9
Thierno <daoudayaboully@yahoo.fr>
2020-05-10 08:58:50 -0400

    Adding sample use of the classifier in a notebook.

--- ML/data_preprocess.py
+++ ML/data_preprocess.py
@@ -4,14 +4,14 @@ lab_enc = LabelEncoder()
 
 
 def data_preprocess1():
-    data = pd.read_csv('./NYPD_Arrest_Data__Year_to_Date_.csv')
+    data = pd.read_csv('C:/Users/daoud/NYPD_Arrest_Data__Year_to_Date_.csv')
     data.drop(['ARREST_KEY', 'PD_CD', 'KY_CD', 'LAW_CODE', ], axis=1, inplace=True)
     data.dropna(inplace=True)
     data.rename(columns={'PD_DESC':'offenses', 'LAW_CAT_CD':'offense_type'}, inplace=True)
 
     X = data[['offenses', 'offense_type', 'ARREST_BORO', 'AGE_GROUP']].values
 
-    X[:,0] = lab_enc.fit_transform(X[:,0])
+    X[:, 0] = lab_enc.fit_transform(X[:,0])
     X[:, 1] = lab_enc.fit_transform(X[:, 1])
     X[:, 2] = lab_enc.fit_transform(X[:, 2])
     X[:, 3] = lab_enc.fit_transform(X[:, 3])
